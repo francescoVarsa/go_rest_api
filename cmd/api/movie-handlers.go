2011a3a1130613ed0623c13ed75e8d869f368ffb
Francesco <francesco.varsallona@gmail.com>
2022-09-03 09:57:44 +0200

    Implement get all movies function

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -22,6 +22,41 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	movie, _ := app.models.DB.Get(id)
 
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
 
-func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {}
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.models.DB.All()
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) isertMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) searchMovies(w http.ResponseWriter, r *http.Request) {
+
+}
